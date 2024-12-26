b29cb78aee2aa44a14667554c973f333fbecd04c
pqsmva <qasimovapervin2004@gmail.com>
2024-12-27 02:38:16 +0400

    main

--- scripts/store/store.js
+++ scripts/store/store.js
@@ -12,13 +12,41 @@ let selectedCats = []
 
 let products = []
 
+let priceFilters = [
+    {
+        min: 0,
+        max: 500
+    },
+    {
+        min: 500,
+        max: 1500
+    },
+    {
+        min: 1500,
+        max: 3000
+    },
+    {
+        min: 3000,
+        max: 5000
+    },
+    {
+        min: 5000,
+        max: 10000
+    },
+
+]
+
+
+let activePrice = null
+
 const filterCategoryWrapper = document.querySelector('.category-filter')
 const productGrid = document.querySelector('.products-grid')
+const priceFiltersWrapper = document.querySelector('.price-filter')
 
 fetchProducts().then(res => {
     products = res
     productTemp = res
-    res.filter(item => ((item.category.toLowerCase()).includes(searchParam.toLowerCase()) || (item.name.toLowerCase()).includes(searchParam.toLowerCase()) )).map(item => {
+    res.filter(item => ((item.category.toLowerCase()).includes(searchParam.toLowerCase()) || (item.name.toLowerCase()).includes(searchParam.toLowerCase()))).map(item => {
 
         productGrid.append(createProduct(item))
 
@@ -43,29 +71,17 @@ fetchProducts().then(res => {
         const checkbox = document.createElement("input");
         checkbox.type = "checkbox";
 
-        checkbox.addEventListener('input', (e)=> {
+        checkbox.addEventListener('input', (e) => {
             if (e.target.checked) {
                 selectedCats.push(cat.name)
+
             } else {
-                selectedCats = selectedCats.filter(x => x!=cat.name)
+                selectedCats = selectedCats.filter(x => x != cat.name)
             }
 
-            productTemp = []
+            applyFilters(searchParam, activePrice)
 
-            selectedCats.map(sc => {
-                productTemp = [...productTemp, ...products.filter(p => p.category == sc)]
-                
-            })
 
-            if (selectedCats.length == 0) {
-                productGrid.innerHTML = ''
-                products.filter(item => item.name.includes(searchParam)).map(item => productGrid.append(createProduct(item)))
-            } else {
-                productGrid.innerHTML = ''
-                productTemp.filter(item => item.name.includes(searchParam)).map(item => productGrid.append(createProduct(item)))
-            }
-            
-            
         })
 
         const span = document.createElement("span");
@@ -79,6 +95,53 @@ fetchProducts().then(res => {
     });
 
 
+    priceFiltersWrapper.innerHTML = ''
+    priceFilters.map(item => {
+        const label = document.createElement("label");
+        label.className = "filter-item";
+    
+        const input = document.createElement("input");
+        input.type = "checkbox";
+        input.setAttribute('min', item.min)
+
+        input.addEventListener('input', (e)=> {
+            console.log(e.target.checked, item.min, item.max);
+            if (e.target.checked) {
+               activePrice = item
+
+               applyFilters(searchParam, activePrice)
+
+               document.querySelectorAll('.price-filter input').forEach(inp => {
+                if (inp.getAttribute('min') == item.min) {
+                    
+                } else {
+                    inp.checked = false
+                }
+               })
+               
+            } else {
+                activePrice = null
+
+            }
+
+            applyFilters(searchParam, activePrice)
+            
+            
+        })
+    
+        const span = document.createElement("span");
+        span.textContent = `${item.min} - ${item.max} Azn`;
+    
+        label.appendChild(input);
+        label.appendChild(span);
+        priceFiltersWrapper.appendChild(label);
+
+
+    });
+
+
+
+
 })
 
 
@@ -86,3 +149,18 @@ fetchProducts().then(res => {
 
 
 
+const applyFilters = (search='', price = null) => {
+    productGrid.innerHTML = ''
+
+    let newList = []
+
+    newList = productTemp.filter(item => (item.name.toLowerCase().includes(search.toLowerCase()) || item.category.toLowerCase().includes(search.toLowerCase())))
+    newList = newList.filter(item => selectedCats.length > 0 ? selectedCats.includes(item.category) : true)
+    newList = newList.filter(item => price ? (item.price > price.min && item.price < price.max): true)
+    
+
+    
+    // [].filter(item => selectedCats !=[] ? selectedCats.includes(item.category) : item.category)
+    // .filter(item => price ? (item.price > price.min && item.price < price.max): item.price > 0)
+    newList.map(item => productGrid.append(createProduct(item)))
+}
